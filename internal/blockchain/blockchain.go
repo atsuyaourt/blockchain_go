c00e4d6c35f6e42d17d6fdbac920add155bd0bf0
Emilio Gozo <15209351+emiliogozo@users.noreply.github.com>
2023-12-28 19:32:35 +0800

    part3: persistence and CLI

--- internal/blockchain/blockchain.go
+++ internal/blockchain/blockchain.go
@@ -1,18 +1,111 @@
 package blockchain
 
+import (
+	"log"
+
+	"github.com/boltdb/bolt"
+)
+
+const (
+	dbFile       = "blockchain.db"
+	blocksBucket = "blocks"
+)
+
 // Blockchain keeps a sequence of Blocks
 type Blockchain struct {
-	Blocks []*Block
+	tip []byte
+	DB  *bolt.DB
+}
+
+// BlockchainIterator is used to iterate over blockchain blocks
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
 }
 
 // NewBlockchain creates a new Blockchain with genesis Block
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Fatalf("db error: %s\n", err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		var err error
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, err = tx.CreateBucket([]byte(blocksBucket))
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			err = b.Put([]byte("l"), genesis.Hash)
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return err
+	})
+
+	if err != nil {
+		log.Fatalf("db error: %s\n", err)
+	}
+
+	bc := Blockchain{tip, db}
+
+	return &bc
 }
 
 // AddBlock saves provided data as a block in the blockchain
 func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.Blocks[len(bc.Blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.Blocks = append(bc.Blocks, newBlock)
+	var lastHash []byte
+
+	err := bc.DB.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("db error: %s\n", err)
+	}
+
+	newBlock := NewBlock(data, lastHash)
+
+	err = bc.DB.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		err = b.Put([]byte("l"), newBlock.Hash)
+		bc.tip = newBlock.Hash
+
+		return err
+	})
+	if err != nil {
+		log.Fatalf("db error: %s\n", err)
+	}
+}
+
+// Iterator ...
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	return &BlockchainIterator{bc.tip, bc.DB}
+}
+
+// Next returns next block starting from the tip
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encBlock)
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("db error: %s\n", err)
+	}
+
+	i.currentHash = block.PrevBlockHash
+
+	return block
 }
